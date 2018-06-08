6b64fd787fdacba9bd1d8ab1ec3c8d35016d89e7
Omniprezz <geoffrey.frioli@gmail.com>
2018-06-08 15:44:48 +0200

    add paint script

--- public/script.js
+++ public/script.js
@@ -1,1 +1,14 @@
 var socket = io.connect('http://localhost:8080');
+
+
+function setup() {
+    createCanvas(710, 400);
+    background(200);
+}
+
+function draw() {
+    stroke(600);
+    if (mouseIsPressed === true) {
+        line(mouseX, mouseY, pmouseX, pmouseY);
+    }
+}
